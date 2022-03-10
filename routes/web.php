ba4b9779d44994e04e26208d061c602574eef845
hiroto <o.hyshrt.19@gmail.com>
2022-03-10 19:53:35 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 
 Route::get('/', [AuthorController::class, 'index']);
-Route::get('/add', [AuthorController::class, 'add']);
-Route::post('/add', [AuthorController::class, 'create']);
